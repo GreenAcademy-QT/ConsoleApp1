58227af8f26a3c7680903ea7ec4ed2fa3e86b0da
MAY6 <MAY6@P506>
2022-12-22 20:45:08 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+void Method(int age, string name, double priceItem) - explicit
+{ 
+
+}
+
+double sum(int number1, int number2)
+{
+    int result = number1 * 10 + number2;
+    int result = number1 * 10 + number2;
+
+
+    int result = number1 * 10 + number2;
+    int result = number1 * 10 + number2;
+
+    if (number1 = number2)
+    {
+        Console.WriteLine("equal");
+    }
+
+    return result;
+}
